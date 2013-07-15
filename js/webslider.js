9b4a442614cbd3df1ae7b1bffab3b529d4c8541b
Adam K Dean <adamkdean@googlemail.com>
2013-07-15 13:28:07 +0100

    Modified style, ready for JS!

--- js/webslider.js
+++ js/webslider.js
@@ -1,7 +1,20 @@
+/* WebSlider (https://github.com/Imdsm/WebSlider)
+ * Adam K Dean (Imdsm)
+ * 15/07/2013
+ */
+
 $(function() {
+
 	// resize slides to full window height
 	$('.slide').css('height', window.innerHeight);
 	$(window).resize(function() {
 		$('.slide').css('height', window.innerHeight);
+	});
+
+	$('.button').click(function() {
+		var target = $(this).data('target');
+
+		//
+
 	});
 });
