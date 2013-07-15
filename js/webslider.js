4951b9105d38806b16cf14af1633d0ffdf109ad1
Adam K Dean <adamkdean@googlemail.com>
2013-07-15 12:53:32 +0100

    Generic names to named names...if that makes sense.

--- js/webslider.js
+++ js/webslider.js
@@ -1,6 +1,5 @@
 $(function() {
-
-	// resize divs
+	// resize slides to full window height
 	$('.slide').css('height', window.innerHeight);
 	$(window).resize(function() {
 		$('.slide').css('height', window.innerHeight);
